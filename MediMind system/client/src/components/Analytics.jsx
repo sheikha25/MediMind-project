f5990c8117da8e8902a80553d91330026464596b
sheikha25 <16S18247@utas.edu.om>
2026-05-17 15:46:12 +0400

    Update Analytics.jsx

--- MediMind system/client/src/components/Analytics.jsx
+++ MediMind system/client/src/components/Analytics.jsx
@@ -1,17 +1,14 @@
 import { Container, Row, Col, Card, CardBody } from "reactstrap";
 import Header from "./Header";
-
 const Analytics = () => {
   return (
     <Container fluid className="dashboard-page">
       <Header />
-
       <Row className="p-4">
         <Col>
           <Card className="dashboard-card text-center">
             <CardBody>
               <h2>Analytics</h2>
-
               <p className="text-muted">
                 Advanced medication adherence analytics and visual reports
               </p>
